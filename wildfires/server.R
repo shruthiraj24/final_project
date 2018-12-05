de2de41c3617e88152f9551c6b7ec6d6b49911af
Shruthi Raj <shruthiraj@D-10-18-167-104.dhcp4.washington.edu>
2018-12-04 23:48:08 -0800

    fix shiny app

--- wildfires/server.R
+++ wildfires/server.R
@@ -17,37 +17,41 @@ library(ggplot2)
 library(R.utils)
 library(rsconnect)
 library(mapdata)
-
+ 
+#Running the shiny input and output & calling in our main dataset
 shinyServer(function(input, output) {
   data <- reactive({
-  ca_wildfires <- data.table::fread("ca-wildfires.csv", 
+  ca_wildfires <- data.table::fread("all-cal-wildfires.csv", 
                                     stringsAsFactors = FALSE)
-  
-  ca_wildfires$latitude <- as.numeric(ca_wildfires$latitude)
-  ca_wildfires$longitude <- as.numeric(ca_wildfires$longitude)
-  ca_wildfires[c(1:100),]
+  ca_wildfires[c(1:50),]
   })
-})
 
+#starting our leaflet plot. In this section, we add markers,tiles and 
+  # create the pops to let viewers focus on. 
 output$caliFireData <- renderLeaflet({
-  ca_wildfires <- data()
+  ca_wildfires <- data() %>% 
+    dplyr::filter(fire_year == input$years)
   
-  wildfires_map <- leaflet(data = ca_wildfires) %>%
+  leaflet(data = ca_wildfires) %>%
     addTiles() %>%
     addMarkers(
-      popup = paste("Offense", ca_wildfires$longitude, "<br>",
-                    "Year:", ca_wildfires$latitude))
-  return(wildfires_map)
+      popup = paste("County", ca_wildfires$county, "<br>",
+                    "Size:", ca_wildfires$fire_size, "acres"))
+  
+  
 })
 
-output$yearSelector <- renderUI({
-  ca_wildfires <- data()
-  selectInput('fire_year',
-              label = 'Year',
-              choices = unique(ca_wildfires$fire_year))
-})
-  
+##output$yearSelector <- renderUI({
+##  ca_wildfires <- data()
+ ## selectInput('fire_year',
+            ##  label = 'Year',
+              ##choices = unique(ca_wildfires$fire_year))
+##})
+
+##generate bar graph with causes  
   output$Plot <- renderPlot({
+    ca_wildfires <- data.table::fread("all-cal-wildfires.csv", 
+                                      stringsAsFactors = FALSE)
     causesYearSelect <- function(choose_year){
       ca_wildfires %>%
         filter(fire_year == as.numeric(choose_year)) %>% 
@@ -55,16 +59,42 @@ output$yearSelector <- renderUI({
         summarize(
           total = n()
         ) %>% 
-        ggplot(aes(x=reorder(stat_cause_descr, total), y=total)) +
+        ggplot(aes(x=reorder(stat_cause_descr, total), y=total, fill = "orange")) +
         geom_bar(stat = "identity") +
         labs(
           x = "Cause of Wildfire",
           y = "Count",
-          title = paste("Wildfires in", as.character(choose_year))
-        ) +
+          title = paste("Wildfires in", as.character(choose_year))) +
+        guides(fill=FALSE)+
         coord_flip()
     }
     causesYearSelect(input$causes)
     
   })
 
+  ## generate map with size and duration of fires  
+  output$map <- renderPlot({
+    ca_wildfires <- data.table::fread("all-cal-wildfires.csv", 
+                                      stringsAsFactors = FALSE)
+    california <- map_data("state", region = "California")
+    sizesYear <- function(select_year){
+      ca_wildfires %>% 
+        filter(fire_year == as.numeric(select_year)) %>% 
+        mutate(fire_length = cont_doy - discovery_doy) %>% 
+    ggplot()+
+      geom_polygon(data = california, aes(x=long, y=lat, group=group))+
+      geom_point(mapping = aes(x= longitude, y= latitude, size= fire_size, color=fire_length) )+
+      scale_color_gradient(low = "royalblue", high = "red3") +
+      scale_size(range =c(0, 5))+
+      scale_size_area(max_size = 5 )+
+      labs(
+        x = "Longitude",
+        y = "Latitude",
+        title = "Duration of Fire and Size", size = "Fire Size", 
+        color= "Duration of Fire") +
+      coord_quickmap()
+    }
+    sizesYear(input$sizes)
+  })
+  
+})
--- wildfires/ui.R
+++ wildfires/ui.R
@@ -18,9 +18,11 @@ library(R.utils)
 library(rsconnect)
 library(mapdata)
  
- ca_wildfires <- data.table::fread("ca-wildfires.csv", stringsAsFactors = F)
+## ca_wildfires <- data.table::fread("all-cal-wildfires.csv", stringsAsFactors = F)
  
- ui <- fluidPage(
+ui <- fluidPage(
+  navbarPage("CA Wildfires", 
+  tabPanel("Leaflet",
    titlePanel("California Under Fire"),
    theme="style.css",
    tags$p(class="important", "This project explores about data of wildfires 
@@ -28,22 +30,56 @@ library(mapdata)
           spread of wildfires in state of California has been a trending topic 
           in the recent time. The data that we've used for this project originially 
           comes from the", tags$a(href="https://www.fs.usda.gov/rds/archive/Product/RDS-2013-0009.4/", 
-          "United States Department of Agriculture Forest Service."), "The dataset
-          contains global unique identifier"), 
+          "United States Department of Agriculture Forest Service."), "The 
+          dataset mainly contains year of wildfire, date of year when the fire 
+          was discovered and stopped, description for cause of the fire, fire
+          size, longitude and latitude of the fire's location. In this project, 
+          we analyzed the location, duration, and cause of the wildfires."), 
    
    sidebarLayout(
      sidebarPanel(
-       
-       uiOutput("yearSelector"), 
-       
-       radioButtons("causes",
-                    "Select a year to display the wildfire causes of that year",
+       radioButtons("years",
+                    "Select a year to display the wildfires of that year",
                     choices = c("1995", "2005", "2015"))
+  #     uiOutput("yearSelector")
+
      ),
-     
      mainPanel(
-       leafletOutput("caliFireData"),
+       leafletOutput("caliFireData")
+   ))),  
+     tabPanel("Causes",
+       titlePanel("Causes of Wildfires"),
+       theme="style.css",
+       p(class="important", "The bar chart below displays the 13 causes of wildfires and the frequency of each given a year.
+             These causes are ordered in descending order to display the most common cause at the top of the graph to the least common cause at the bottom.
+             "),
+       sidebarLayout(
+         sidebarPanel(
+           radioButtons("causes",
+                        "Select a year to display the wildfire causes of that year",
+                        choices = c("1995", "2005", "2015"))
+         ),
+      mainPanel(    
        plotOutput("Plot")
      )
    )
-)
+),
+tabPanel("Size and Duration",
+         titlePanel("Size and Duration of Fires"),
+         theme="style.css",
+         p(class="important", "The bar chart below displays the 13 causes of wildfires and the frequency of each given a year.
+           These causes are ordered in descending order to display the most common cause at the top of the graph to the least common cause at the bottom.
+           "),
+         sidebarLayout(
+           sidebarPanel(
+             radioButtons("sizes",
+                          "Select a year to display the wildfire sizes and durations of that year",
+                          choices = c("1995", "2005", "2015"))
+           ),
+           mainPanel(    
+             plotOutput("map")
+           )
+         )
+         )
+
+))
